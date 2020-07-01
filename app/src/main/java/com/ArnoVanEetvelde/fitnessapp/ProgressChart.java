c7a96770ef6f184d68febe1362cd9cc1e9a7920d
arno.vaneetvelde <arno.vaneetvelde@student.kuleuven.be>
2020-07-01 18:09:26 +0200

    Main page

---
+++ app/src/main/java/com/ArnoVanEetvelde/fitnessapp/ProgressChart.java
@@ -0,0 +1,17 @@
+package com.ArnoVanEetvelde.fitnessapp;
+
+import java.util.HashMap;
+
+public class ProgressChart {
+
+    private HashMap<Integer, Integer> data;
+
+    public ProgressChart(HashMap<Integer, Integer> data){
+        this.data = data;
+    }
+
+    public GraphicalView getChartView(){
+
+    }
+
+}
