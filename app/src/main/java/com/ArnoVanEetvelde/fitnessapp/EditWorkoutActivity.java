ecc493fa5a3ffd31941113155d8108d99bd882b9
arno.vaneetvelde <arno.vaneetvelde@student.kuleuven.be>
2020-07-19 10:45:53 +0200

    EditWorkout Screen

---
+++ app/src/main/java/com/ArnoVanEetvelde/fitnessapp/EditWorkoutActivity.java
@@ -0,0 +1,14 @@
+package com.ArnoVanEetvelde.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditWorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_workout);
+    }
+}
